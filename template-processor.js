000b88b6a0f4b51f4f3b5b5bf9867b47722790ba
Anusha Bethini <anushabethini@gmail.com>
2024-02-02 10:47:10 -0500

    template processor js file commit

---
+++ template-processor.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function TemplateProcessor(template) {
+    this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function(dictionary) {
+    return this.template.replace(/{{(.*?)}}/g, function(match, property) {
+        return dictionary[property] || '';
+    });
+};
